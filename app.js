b7347c57bc52cd9fe0130ca2dac68824b9dcd495
ledcom <valonibrahimi09@gmail.com>
2023-06-15 00:17:59 +0200

    App Kosovo

---
+++ app.js
@@ -0,0 +1,6 @@
+ const hamburger_menu = document.querySelector(".hamburger-menu");
+ const permbajtja = document.querySelector(".permbajtja");
+
+hamburger_menu.addEventListener("click", () => {
+    permbajtja.classList.toggle("active");
+});
